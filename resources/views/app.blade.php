b9ae910d87726ad3ecb2f3dfaf2e31f3c6a4884d
Jonas Hamm <contact@jonashamm.de>
2016-04-26 15:58:53 +0200

    initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    Wir laden auch diese Hauptdatei.
+    <br><br>
+    @yield('content')
+</div>
+</body>
+</html>
