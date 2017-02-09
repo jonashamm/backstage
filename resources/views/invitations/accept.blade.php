0b6e6e63fceabf4641de6e1f509d7afd256f6fdd
Jonas Hamm <contact@jonashamm.de>
2017-02-09 10:58:10 +0100

    label password fields

--- resources/views/invitations/accept.blade.php
+++ resources/views/invitations/accept.blade.php
@@ -1,25 +1,36 @@
 @extends('app')
 
 @section('content')
-    <h3>Bitte lege dein Passwort fest</h3>
+    <div class="accept-invitation">
+        <div class="inner">
 
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+
+            <h3>Bitte lege ein (neues) Passwort fest</h3>
+
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <form action="{{url('/')}}/redeem-invitation" method="post">
+                {{ csrf_field() }}
+                <div class="formfield">
+                    <label for="password">Neues Passwort</label>
+                    <input type="password" name="password" placeholder="Neues Passwort">
+                </div>
+                <div class="formfield">
+                    <label for="password_confirmation">Neues Passwort wiederholen</label>
+                    <input type="password" name="password_confirmation" placeholder="Neues Passwort wiederholen">
+                </div>
+                <input type="hidden" name="code" value="{{ $code }}">
+                <input type="hidden" name="user_id" value="{{ $user_id }}">
+                <button>Passwort speichern</button>
+            </form>
         </div>
-    @endif
-
-    <form action="{{url('/')}}/redeem-invitation" method="post">
-        {{ csrf_field() }}
-        <input type="password" name="password">
-        <input type="password" name="password_confirmation">
-        <input type="hidden" name="code" value="{{ $code }}">
-        <input type="hidden" name="user_id" value="{{ $user_id }}">
-        <button>Passwort speichern</button>
-    </form>
-
+    </div>
 @endsection
