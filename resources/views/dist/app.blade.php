393e0c5c94cf2d55821247941490f62151ceca1c
Jonas Hamm <contact@jonashamm.de>
2017-02-24 12:06:22 +0100

    song add toggler relative pos

--- resources/views/dist/app.blade.php
+++ resources/views/dist/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>backstage</title>
     <link rel="stylesheet" href="https://cdn.plyr.io/2.0.11/plyr.css">
-    <link rel="stylesheet" href="{{ asset('dist/all-styles.min.css?v=1487933910742') }}">
+    <link rel="stylesheet" href="{{ asset('dist/all-styles.min.css?v=1487934370624') }}">
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{url('/')}}/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="{{url('/')}}/favicon-32x32.png" sizes="32x32">
@@ -47,7 +47,7 @@
     </header>
     @yield('content')
 </div>
-<script src="{{ asset('dist/all-vendor-scripts.js?v=1487933910742') }}"></script>
-<script src="{{ asset('dist/custom.js?v=1487933910742') }}"></script>
+<script src="{{ asset('dist/all-vendor-scripts.js?v=1487934370624') }}"></script>
+<script src="{{ asset('dist/custom.js?v=1487934370624') }}"></script>
 </body>
 </html>
